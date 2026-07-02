2c36a9af58b9b1d284ba8bd28bc403747408c766
dhcho0422 <dhcho0422@ttam.ai>
2026-07-02 09:16:47 +0900

    Feat: ChartStyle 및 ChartLayerBuilder 레이어 트리 조립

---
+++ ios-renderer/Sources/LumipolGraphUI/ChartStyle.swift
@@ -0,0 +1,43 @@
+import UIKit
+
+/// 차트 팔레트·타이포·여백. 앱이 통째로 주입해 커스터마이징한다(기본값 제공).
+public struct ChartStyle {
+    // 시리즈 라인
+    public var lineWidth: CGFloat = 2
+    public var primaryLineColor: UIColor = .systemBlue
+    public var secondaryLineColor: UIColor = .systemRed
+    /// main 시리즈 라인색 기반 area 그라데이션의 시작 알파. 0이면 그라데이션 없음.
+    public var gradientMaxAlpha: CGFloat = 0.25
+
+    // 고스트(지난 러닝)
+    public var ghostLineColor: UIColor = UIColor.systemGray.withAlphaComponent(0.7)
+    public var ghostLineWidth: CGFloat = 1.5
+    public var ghostDashPattern: [NSNumber] = [4, 3]
+
+    // 기준선/밴드
+    public var refLineColor: UIColor = .systemOrange
+    public var refLineDashPattern: [NSNumber] = [6, 3]
+    public var refBandColor: UIColor = UIColor.systemOrange.withAlphaComponent(0.12)
+
+    // 구간(km) 마커
+    public var markerLineColor: UIColor = .systemGray4
+    public var markerEmphasisLineColor: UIColor = .systemGray
+
+    // 축 라벨
+    public var axisLabelFont: UIFont = .systemFont(ofSize: 10)
+    public var axisLabelColor: UIColor = .secondaryLabel
+
+    // 플롯 여백 (좌우 = Y축 라벨, 상하 = 마커 라벨/X축 라벨)
+    public var plotInsets: UIEdgeInsets = UIEdgeInsets(top: 16, left: 44, bottom: 20, right: 44)
+
+    // 터치 마커
+    public var touchLineColor: UIColor = .label
+    public var touchDotRadius: CGFloat = 4
+    public var bubbleBackgroundColor: UIColor = UIColor.label.withAlphaComponent(0.85)
+    public var bubbleTextColor: UIColor = .systemBackground
+    public var bubbleFont: UIFont = .systemFont(ofSize: 11, weight: .medium)
+
+    public init() {}
+
+    public static let `default` = ChartStyle()
+}
